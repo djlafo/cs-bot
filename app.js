e28aeece233244d99689d6b492961fb6e3bf1b72
dylan.lafont <dylan.lafont@mediware.com>
2019-08-08 17:40:42 -0500

    print body

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ if(process.env.PRODUCTION) {
 server.post('/api/v1/bot/messages', connector.listen());
 
 server.post('/api/v1/bot/merge_requests', (req, res, next) => {
-  console.log(req);
+  console.log(req.body);
   res.send();
   return next();
 });
