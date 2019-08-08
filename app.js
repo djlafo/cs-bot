c9adc802251e679ca365a85f6b23cd7c3627e938
dylan.lafont <dylan.lafont@mediware.com>
2019-08-08 14:08:59 -0500

    try new way

--- app.js
+++ app.js
@@ -15,34 +15,40 @@ const connector = new teams.TeamsChatConnector({
   appPassword: appPassword
 });
 
+const inMemoryBotStorage = new builder.MemoryBotStorage();
+
 const server = restify.createServer();
 server.listen(process.env.PORT, function () {
     console.log('%s listening to %s', server.name, util.inspect(server.address()));
 });
 
 // this will receive nothing, you can put your tenant id in the list to listen
-connector.setAllowedTenants([]);
+// connector.setAllowedTenants([]);
 // this will reset and allow to receive from any tenants
 connector.resetAllowedTenants();
 
 server.post('/api/v1/bot/messages', connector.listen());
 
-const bot = new builder.UniversalBot(connector);
+const bot = new builder.UniversalBot(connector, (session) => {
+  // Message might contain @mentions which we would like to strip off in the response
+  const text = teams.TeamsMessage.getTextWithoutMentions(session.message);
+  session.send('You said: %s', text);
+}).set('storage', inMemoryBotStorage);
 
-bot.dialog('/', [
-  function (session) {
-      builder.Prompts.choice(session, "Choose an option:", 'Fetch channel list|Mention user|Start new 1 on 1 chat|Route message to general channel|FetchMemberList|Send O365 actionable connector card|FetchTeamInfo(at Bot in team)|Start New Reply Chain (in channel)|Issue a Signin card to sign in a Facebook app|Logout Facebook app and clear cached credentials|MentionChannel|MentionTeam|NotificationFeed|Bot Delete Message');
-  },
-  function (session, results) {
-    switch (results.response.index) {
-    }
-  }
-]);
+// bot.dialog('/', [
+//   function (session) {
+//       builder.Prompts.choice(session, "Choose an option:", 'Fetch channel list|Mention user|Start new 1 on 1 chat|Route message to general channel|FetchMemberList|Send O365 actionable connector card|FetchTeamInfo(at Bot in team)|Start New Reply Chain (in channel)|Issue a Signin card to sign in a Facebook app|Logout Facebook app and clear cached credentials|MentionChannel|MentionTeam|NotificationFeed|Bot Delete Message');
+//   },
+//   function (session, results) {
+//     switch (results.response.index) {
+//     }
+//   }
+// ]);
 
-bot.on('conversationUpdate', function (message) {
-  console.log(message);
-  const event = teams.TeamsMessage.getConversationUpdateData(message);
-  session.endDialog('hello');
-});
+// bot.on('conversationUpdate', function (message) {
+//   console.log(message);
+//   const event = teams.TeamsMessage.getConversationUpdateData(message);
+//   session.endDialog('hello');
+// });
 
 
